1f32ba8ec1ac2408c6885df117bb3dc81e37ca48
Jasmin Bom <jsmnbom@gmail.com>
2019-10-29 16:02:37 +0100

    Disable debug features

--- Singletons/Debug.gd
+++ Singletons/Debug.gd
@@ -1,28 +1,28 @@
 extends Node
 
 # Gives 10 of each inventory item
-var INVENTORY = true
+var INVENTORY = false
 
 # Makes plants grow 60 times faster (min 5 sec)
-var FAST_PLANTS = true
+var FAST_PLANTS = false
 
 # Gives you 100 000 000 gems from the start
 var GEMS = false
 
 # Makes weeds grow very fast
-var WEEDS = true
+var WEEDS = false
 
 # Makes fields dry out much faster
 var DRY = false
 
 # Starts all fields wet
-var START_WET = true
+var START_WET = false
 
 # Set achivement values to 150
 var ACHIEVEMENTS = false
 
 # Unlock all journal pages
-var JOURNAL = true
+var JOURNAL = false
 
 # Day+night is only 30 sec
 var FAST_DAY = false
