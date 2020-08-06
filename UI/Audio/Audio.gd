eb32228bb7d1766032fb68ffdc7654694294c537
Jasmin Bom <jsmnbom@gmail.com>
2020-08-06 15:17:34 +0200

    Add ambience

--- UI/Audio/Audio.gd
+++ UI/Audio/Audio.gd
@@ -5,7 +5,7 @@ const MUSIC = 1
 const SFX = 2
 const AMBIANCE = 3
 
-var current_song = 'Midnight in the Garden'
+var current_music = 'Midnight in the Garden'
 
 var music = {
 	'Midnight in the Garden': preload('res://assets/audio/music/midnight_in_the_garden.ogg'),
@@ -17,12 +17,46 @@ func _ready():
 	$Music.connect('finished', self, '_on_music_finished')
 	$MusicNowPlayingLabel.modulate.a = 0
 	play_music()
+	
+	$DayTimer.wait_time = float(Data.day_duration) / (24.0*60)
+	$DayTimer.start()
+	_on_DayTimer_timeout()
+
+func set_ambience(day, night):
+	if day > 0.0:
+		if not $AmbienceDay.playing:
+			$AmbienceDay.playing = true
+		$AmbienceDay.volume_db = linear2db(day)
+	elif $AmbienceDay.playing:
+		$AmbienceDay.playing = false
+	if night > 0.0:
+		if not $AmbienceNight.playing:
+			$AmbienceNight.playing = true
+		$AmbienceNight.volume_db = linear2db(night)
+	elif $AmbienceNight.playing:
+		$AmbienceNight.playing = false
+
+func _on_DayTimer_timeout():
+	Data.time = (Data.time + 1) % (24*60)
+	
+	var t = 0.0
+	
+	if Data.time > 21*60 or Data.time < 6*60:
+		set_ambience(0, 1)
+	elif (Data.time < 9*60):
+		t = range_lerp(float(Data.time), 6*60, 9*60, 0.0, 1.0)
+		set_ambience(t, 1.0-t)
+	elif (Data.time < 18*60):
+		set_ambience(1, 0)
+	elif (Data.time <= 21*60):
+		t = range_lerp(float(Data.time), 18*60, 21*60, 1.0, 0.0)
+		set_ambience(t, 1.0-t)
 
 func random_music():
 	return music.keys()[Utils.rng.randi() % music.size()]
 
 func play_music():
-	$Music.stream = music[current_song]
+	$Music.stream = music[current_music]
 	$Music.play(0.0)
 
 	if AudioServer.get_bus_volume_db(MASTER) != -INF and AudioServer.get_bus_volume_db(MUSIC) != -INF:
@@ -30,7 +64,7 @@ func play_music():
 
 func show_music_now_playing_label():
 	var label = $MusicNowPlayingLabel
-	label.set_text('Now playing: %s' % current_song)
+	label.set_text('Now playing: %s' % current_music)
 	$Tween.interpolate_property(label, 'modulate:a',
 		0.0, 1.0, 0.5,
 		Tween.TRANS_LINEAR, Tween.EASE_IN)
@@ -41,9 +75,9 @@ func show_music_now_playing_label():
 
 func _on_music_finished():
 	var new_song = random_music()
-	while new_song == current_song:
+	while new_song == current_music:
 		new_song = random_music()
-	current_song = new_song
+	current_music = new_song
 	play_music()
 
 func set_volume(bus, volume):
